d7713bdbc2ca8ae5129f7e5d5e9c0ed8af07e731
Vasudeva8278 <111454632+Vasudeva8278@users.noreply.github.com>
2025-07-11 11:47:07 +0530

    frist commit

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(express.json());
 app.use(cors());
 
+
+
+
+app.get("/", (req, res) => {
+  return res.json({ message: "get success running" });
+});
+
+
 app.use(session({
   secret: process.env.SESSION_SECRET || 'your-secret-key',
   resave: false,
